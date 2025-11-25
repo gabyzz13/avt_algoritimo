10e0bc3f1c9ef74d5fae67a87251a76e8512498b
gabyzz13 <gabrielagomesbz13@gmail.com>
2025-11-24 21:49:42 -0300

    Create questao_06.py

---
+++ questao_06.py
@@ -0,0 +1,15 @@
+def f(x=100):
+    from itertools import product
+    numbrs = "123456789"
+    operadores = ["", "+", "-"]
+    result = []
+    
+    for ops in product(operadores, repeat=len(numbrs)-1):
+        expr = "".join(num + op for num, op in zip(numbrs, ops + ("",)))
+        if eval(expr) == x:
+            result.append(expr + "==" + str(x))
+    return result
+
+#Utilizando os exemplos da questão
+for eq in f(100):
+    print(eq)
